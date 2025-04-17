2092d7b4e03ef2afbc405fe5b225432f54ae054b
MOHAMMED MOIZ <iammohammedmoiz@gmail.com>
2025-04-17 09:02:48 +0530

    up

--- src/contexts/BagContext.tsx
+++ src/contexts/BagContext.tsx
@@ -1,32 +1,18 @@
 import React, { createContext, useContext, useReducer, ReactNode } from 'react';
-import { Movie, UserBag } from '../types/UserBag';
+import { Movie, UserBag, BagAction } from '../types/UserBag';
 
-type BagAction =
-  | { type: 'ADD_TO_WATCHLIST'; payload: Movie }
-  | { type: 'REMOVE_FROM_WATCHLIST'; payload: Movie }
-  | { type: 'ADD_TO_FAVORITES'; payload: Movie }
-  | { type: 'REMOVE_FROM_FAVORITES'; payload: Movie }
-  | { type: 'ADD_TO_WATCHED'; payload: Movie }
-  | { type: 'REMOVE_FROM_WATCHED'; payload: Movie };
-
-interface BagState {
-  watchlist: Movie[];
-  favorites: Movie[];
-  watched: Movie[];
-}
-
-const initialState: BagState = {
+const initialState: UserBag = {
   watchlist: [],
   favorites: [],
   watched: []
 };
 
 const BagContext = createContext<{
-  bag: BagState;
+  bag: UserBag;
   dispatch: React.Dispatch<BagAction>;
 } | undefined>(undefined);
 
-function bagReducer(state: BagState, action: BagAction): BagState {
+function bagReducer(state: UserBag, action: BagAction): UserBag {
   switch (action.type) {
     case 'ADD_TO_WATCHLIST':
       return {
--- src/types/UserBag.ts
+++ src/types/UserBag.ts
@@ -8,9 +8,15 @@ export interface Movie {
 }
 
 export interface UserBag {
-  movies: Movie[];
-  addMovie: (movie: Movie) => void;
-  removeMovie: (movieId: number) => void;
-  clearBag: () => void;
-  isInBag: (movieId: number) => boolean;
-} 
+  watchlist: Movie[];
+  favorites: Movie[];
+  watched: Movie[];
+}
+
+export type BagAction =
+  | { type: 'ADD_TO_WATCHLIST'; payload: Movie }
+  | { type: 'REMOVE_FROM_WATCHLIST'; payload: Movie }
+  | { type: 'ADD_TO_FAVORITES'; payload: Movie }
+  | { type: 'REMOVE_FROM_FAVORITES'; payload: Movie }
+  | { type: 'ADD_TO_WATCHED'; payload: Movie }
+  | { type: 'REMOVE_FROM_WATCHED'; payload: Movie }; 
